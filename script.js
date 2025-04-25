fd5bb2588faf32cdc401f3fc74a780f5723c003f
adwaita shinde <sadwaita2001@gmail.com>
2025-04-25 17:05:06 +0330

    Updated styles, scripts, or images

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-$(document).ready(function () {
-    $("#orderBtn").click(function () {
-      alert("Thanks for placing your order! 🛵🍕");
-    });
+$(document).ready(function(){
+  $("#orderBtn").click(function(){
+    alert("Thanks for placing your order! \uD83D\uDEF5\uD83C\uDF55")}),
   
-    $(".offer-box").click(function () {
-      let offer = $(this).data("offer");
-      alert(offer); 
-    });
+  $(".offer-box").click(function(){
+    alert($(this).data("offer"))}),
   
-    $(".offer-box").hover(
-      function () {
-        $(this).css("background-color", "#ffd580");
-      },
-      function () {
-        $(this).css("background-color", "#ffe1b3");
-      }
-    );
-  
-    $('#slide-box').slideDown(1000);
+  $(".offer-box").hover(function(){
+    $(this).css("background-color","#ffd580")},
+    
+    function(){$(this).css("background-color","#ffe1b3")}),
+    
+    $("#slide-box").slideDown(1e3)
   });
-  
